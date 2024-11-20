0ff4cf7a6dac1229d4ca4ddcf60404091b22ae72
milosz.niewiarowski.4@gmail.com <milosz.niewiarowski.4@gmail.com>
2024-11-20 19:00:55 +0100

    Adding pagination to reviews of beers

--- BeerRateApi/Controllers/BeerReviewController.cs
+++ BeerRateApi/Controllers/BeerReviewController.cs
@@ -13,6 +13,7 @@ namespace BeerRateApi.Controllers
     public class BeerReviewController : Controller
     {
         public readonly IBeerReviewService _beerReviewService;
+        private const int reviewsPerPage = 20;
         public BeerReviewController(IBeerReviewService beerReviewService)
         {
             _beerReviewService = beerReviewService;
@@ -79,9 +80,37 @@ namespace BeerRateApi.Controllers
             catch (Exception ex)
             {
                 return StatusCode(500, new { ex.Message });
-
             }
         }
+        [AllowAnonymous]
+        [HttpGet("getbeerreviewspage/{id}")]
+        public async Task<IActionResult> GetBeerReviewsPage(int id, int page)
+        {
 
+            try
+            {
+                var reviews = await _beerReviewService.GetBeerReviews(id, (page-1)*reviewsPerPage, reviewsPerPage * page-1);
+                return Ok(new { Reviews = reviews });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { ex.Message });
+            }
+        }
+        [HttpGet("getbeerreviewpagesamount/{id}")]
+        public async Task<IActionResult> GetBeerReviewPagesAmount(int id, int page)
+        {
+
+            try
+            {
+                var counter = await _beerReviewService.GetReviewsCounter(id);
+                int result = counter % reviewsPerPage == 0? counter /reviewsPerPage : counter / reviewsPerPage+1;
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { ex.Message });
+            }
+        }
     }
 }
