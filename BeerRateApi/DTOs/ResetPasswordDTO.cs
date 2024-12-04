67e138244c284fd0c959682baccbbd792de72f05
KubaDD <kuburolek11@gmail.com>
2024-12-04 22:05:38 +0100

    Reset password fixes

---
+++ BeerRateApi/DTOs/ResetPasswordDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BeerRateApi.DTOs
+{
+    public class ResetPasswordDTO
+    {
+        public string NewPassword { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+    }
+}
